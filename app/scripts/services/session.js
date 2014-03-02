47cb21699d26d6e9831c24ef201603877507c8e7
yoann <yoann.prot@gmail.com>
2014-03-02 15:04:30 +0100

    Big bang again :)

--- app/scripts/services/session.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('bestBottleApp')
-  .factory('Session', function ($resource) {
-    return $resource('/api/session/');
-  });
--- server.js
+++ server.js
@@ -1,49 +1,82 @@
 'use strict';
 
+/**
+ * Module dependencies.
+ */
 var express = require('express'),
-    path = require('path'),
     fs = require('fs'),
-    mongoose = require('mongoose');
+    passport = require('passport'),
+    logger = require('mean-logger');
 
 /**
- * Main application file
+ * Main application entry file.
+ * Please note that the order of loading is important.
  */
 
-// Set default node environment to development
+// Load configurations
+// Set the node enviornment variable if not set before
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-// Application Config
-var config = require('./lib/config/config');
+// Initializing system variables 
+var config = require('./config/config'),
+    mongoose = require('mongoose');
 
-// Connect to database
-var db = mongoose.connect(config.mongo.uri, config.mongo.options);
+// Bootstrap db connection
+var db = mongoose.connect(config.db);
 
 // Bootstrap models
-var modelsPath = path.join(__dirname, 'lib/models');
-fs.readdirSync(modelsPath).forEach(function (file) {
-  if (/(.*)\.(js$|coffee$)/.test(file)) {
-    require(modelsPath + '/' + file);
-  }
-});
+var models_path = __dirname + '/app/models';
+var walk = function(path) {
+    fs.readdirSync(path).forEach(function(file) {
+        var newPath = path + '/' + file;
+        var stat = fs.statSync(newPath);
+        if (stat.isFile()) {
+            if (/(.*)\.(js$|coffee$)/.test(file)) {
+                require(newPath);
+            }
+        } else if (stat.isDirectory()) {
+            walk(newPath);
+        }
+    });
+};
+walk(models_path);
 
-// Populate empty DB with sample data
-require('./lib/config/dummydata');
-  
-// Passport Configuration
-var passport = require('./lib/config/passport');
+// Bootstrap passport config
+require('./config/passport')(passport);
 
 var app = express();
 
 // Express settings
-require('./lib/config/express')(app);
+require('./config/express')(app, passport, db);
 
-// Routing
-require('./lib/routes')(app);
+// Bootstrap routes
+var routes_path = __dirname + '/app/routes';
+var walk = function(path) {
+    fs.readdirSync(path).forEach(function(file) {
+        var newPath = path + '/' + file;
+        var stat = fs.statSync(newPath);
+        if (stat.isFile()) {
+            if (/(.*)\.(js$|coffee$)/.test(file)) {
+                require(newPath)(app, passport);
+            }
+        // We skip the app/routes/middlewares directory as it is meant to be
+        // used and shared by routes as further middlewares and is not a 
+        // route by itself
+        } else if (stat.isDirectory() && file !== 'middlewares') {
+            walk(newPath);
+        }
+    });
+};
+walk(routes_path);
 
-// Start server
-app.listen(config.port, function () {
-  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
-});
+
+// Start the app by listening on <port>
+var port = process.env.PORT || config.port;
+app.listen(port);
+console.log('Express app started on port ' + port);
+
+// Initializing logger
+logger.init(app, passport, mongoose);
 
 // Expose app
 exports = module.exports = app;
