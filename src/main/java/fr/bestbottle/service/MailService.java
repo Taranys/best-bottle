15ff6370bb182dcb769d9a3bf3f662a758159017
yoann <yoann.prot@afp.com>
2014-08-21 18:09:05 +0200

    Add log on mail service

--- src/main/java/fr/bestbottle/service/MailService.java
+++ src/main/java/fr/bestbottle/service/MailService.java
@@ -25,51 +25,52 @@ import java.util.Locale;
 @Service
 public class MailService {
 
-  private final Logger log = LoggerFactory.getLogger(MailService.class);
+    private final Logger log = LoggerFactory.getLogger(MailService.class);
 
-  @Inject
-  private Environment env;
+    @Inject
+    private Environment env;
 
-  @Inject
-  private JavaMailSenderImpl javaMailSender;
+    @Inject
+    private JavaMailSenderImpl javaMailSender;
 
-  @Inject
-  private MessageSource messageSource;
+    @Inject
+    private MessageSource messageSource;
 
-  /**
-   * System default email address that sends the e-mails.
-   */
-  private String from;
+    /**
+     * System default email address that sends the e-mails.
+     */
+    private String from;
 
-  @PostConstruct
-  public void init() {
-    this.from = env.getProperty("spring.mail.from");
-  }
-
-  @Async
-  public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
-    log.debug("Send e-mail[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
-            isMultipart, isHtml, to, subject, content);
-
-    // Prepare message using a Spring helper
-    MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-    try {
-      MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, CharEncoding.UTF_8);
-      message.setTo(to);
-      message.setFrom(from);
-      message.setSubject(subject);
-      message.setText(content, isHtml);
-      javaMailSender.send(mimeMessage);
-      log.debug("Sent e-mail to User '{}'!", to);
-    } catch (Exception e) {
-      log.warn("E-mail could not be sent to user '{}', exception is: {}", to, e.getMessage());
+    @PostConstruct
+    public void init() {
+        this.from = env.getProperty("spring.mail.from");
+        log.info("From address for mail : {}", from);
     }
-  }
 
-  @Async
-  public void sendActivationEmail(final String email, String content, Locale locale) {
-    log.debug("Sending activation e-mail to '{}'", email);
-    String subject = messageSource.getMessage("email.activation.title", null, locale);
-    sendEmail(email, subject, content, false, true);
-  }
+    @Async
+    public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
+        log.debug("Send e-mail[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
+                isMultipart, isHtml, to, subject, content);
+
+        // Prepare message using a Spring helper
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, CharEncoding.UTF_8);
+            message.setTo(to);
+            message.setFrom(from);
+            message.setSubject(subject);
+            message.setText(content, isHtml);
+            javaMailSender.send(mimeMessage);
+            log.debug("Sent e-mail to User '{}'!", to);
+        } catch (Exception e) {
+            log.warn("E-mail could not be sent to user '{}', exception is: {}", to, e.getMessage());
+        }
+    }
+
+    @Async
+    public void sendActivationEmail(final String email, String content, Locale locale) {
+        log.debug("Sending activation e-mail to '{}'", email);
+        String subject = messageSource.getMessage("email.activation.title", null, locale);
+        sendEmail(email, subject, content, false, true);
+    }
 }
